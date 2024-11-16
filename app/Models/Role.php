685ff6f09a6575de1d334fea4c3fa4a8105e217a
Joseph <182897@unsaac.edu.pe>
2024-11-15 19:51:42 -0500

    Asignacion de roles por medio de una rutaç

---
+++ app/Models/Role.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+    protected $table = 'TRol';
+    protected $primaryKey = 'id_rol';
+    public $incrementing = false;
+    protected $fillable = ['id_rol', 'tipo_rol', 'descripcion'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'TUsuarioRoles', 'id_rol', 'id_usuario', 'id_rol', 'id_usuario');
+    }
+    public static function generateRoleId()
+    {
+        $lastRole = self::orderBy('id_rol', 'desc')->first();
+        if (!$lastRole) {
+            return 'R001';
+        }
+        $lastIdNumber = intval(substr($lastRole->id_rol, 1));
+        $newIdNumber = $lastIdNumber + 1;
+        return 'R' . str_pad($newIdNumber, 3, '0', STR_PAD_LEFT);
+    }
+}
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+         Role::create([
+            'id_rol' => Role::generateRoleId(),
+            'tipo_rol' => 'DOCENTE',
+            'descripcion' => 'Rol de docente',
+        ]);
+
+        Role::create([
+            'id_rol' => Role::generateRoleId(),
+            'tipo_rol' => 'SUPERVISOR',
+            'descripcion' => 'Rol de supervisor',
+        ]);
+
+        Role::create([
+            'id_rol' => Role::generateRoleId(),
+            'tipo_rol' => 'ADMINISTRADOR',
+            'descripcion' => 'Rol de administrador',
+        ]);
+    }
+}
